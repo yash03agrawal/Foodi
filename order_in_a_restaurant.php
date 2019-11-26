3dfcf2812faac8ca088460d48cf5e039782955ec
Yash Agrawal <43888404+yash03agrawal@users.noreply.github.com>
2019-11-26 22:45:27 +0530

    Add files via upload

---
+++ order_in_a_restaurant.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+         <meta name="author" content="Yash Agrawal and Shivam Kalhans">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="shortcut icon" href="icon.ico" />
+        <title>
+            FOODI
+        </title>
+		
+ <?php 
+		
+    session_start();
+    if(empty($_SESSION['name']))
+    {
+        header("Location:login.php");
+    }
+    else if($_SESSION['type']==0)
+    {
+ ?>
+		
+	 </head>
+    <body>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="admin.php"><i class="fas fa-hamburger"></i> FOODI</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                     <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+
+              <div class="collapse navbar-collapse" id="navbarContent">
+                
+                <ul class="navbar-nav ml-auto nav-right">
+                  <li class="nav-item dropdown">    
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="fas fa-user-cog"></i> <?php
+                            echo $_SESSION["name"] ;
+                        ?>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+        </nav>
+		
+		<div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card">
+                    
+                    <article class="card-body">
+                    <form method="get" action="order_in_a_restaurant_result.php">
+                        <div class="form-row">
+                            <div class="col form-group">
+                                <label for="search">Enter Date</label>
+                                <input type="date" class="form-control" name="search" placeholder="YYYY-MM-DD" required>
+                            </div> <!-- form-group end.// -->
+                        </div>
+						<div class="form-row">
+							<div class="col form-group">
+								<select id="restaurant" class="form-control" name="restaurant" required>
+									  <option value="1">Andhra Vantillu</option>
+									  <option value="2">Beijing Eats</option>
+									  <option value="3">Don's Pizza</option>
+									  <option value="4">Everest</option>
+									  <option value="5">Good Bowl</option>
+									  <option value="6">Kai Foods</option>
+								</select>
+							</div>
+						</div>	
+						<div class="form-group">
+                            <button type="submit" id="search" class="btn btn-primary btn-block"> Search  </button>
+                        </div> <!-- form-group// --> 
+                    </form>
+                    </article> <!-- card-body end .// -->
+                    </div> <!-- card.// -->
+                </div> <!-- col.//-->
+            </div> <!-- row.//-->
+        </div>
+		
+		
+		 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		
+	</body><?php }
+	else
+	{
+		header("Location:access_deny.html");
+	}
+	?>
+</html>
